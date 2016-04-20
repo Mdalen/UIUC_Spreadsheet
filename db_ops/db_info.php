4ac1acc40bcfe75959b9d2adf86b19b191f563d7
Mdalen <mdalen@iwu.edu>
2016-04-20 12:10:51 -0500

    Update db_info.php

--- db_ops/db_info.php
+++ db_ops/db_info.php
@@ -3,7 +3,7 @@
 //this is a config file for the project list site
 //edit this instead of messing with the arguments on the PDO statement
 $db_host 	= "localhost";
-$db_name	= "spreadsheet";
-$db_user	= "mdalen";
-$db_pass	= "BlueMoon";
+$db_name	= "";
+$db_user	= "";
+$db_pass	= "";
 $db_charset	= "UTF8";
